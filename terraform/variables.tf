1a495229f38aa460e419480142ee93581658e4ca
pcbarretos <paullo.barreto@gmail.com>
2024-09-04 16:05:58 -0300

    Fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,18 @@ variable "cluster_name" {
   type        = string
   default     = "tech-challange"
 }
+
+variable "bucket_key" {
+  description = "Ky Bucket"
+  type = string  
+}
+
+variable "dynamodb_table" {
+  description = "DynamoDb table to Lock State"
+  type = string  
+}
+
+variable "bucket_state_name" {
+  description = "Bucket State Name"
+  type = string  
+}
