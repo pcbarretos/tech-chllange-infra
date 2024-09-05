6d1c394ac694f50261e6ac4bf6b4796f82096328
pcbarretos <paullo.barreto@gmail.com>
2024-09-05 15:51:28 -0300

    Update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,27 @@
 variable "vpc_name" {
   description = "Name of the VPC"
   type        = string
-  default     = "prod-vpc"
 }
 
 variable "cluster_name" {
   description = "Name of your k8s cluster"
   type        = string
-  default     = "tech-challange"
+}
+
+variable "cidr" {
+  description = "Cidr"
+  type        = string
+  
+}
+
+variable "public_subnets" {
+  description = "list of Public Subnets"
+  type = list(string)
+  
+}
+
+variable "private_subnets" {
+  description = "list of Private Subnets"
+  type = list(string)
+  
 }
