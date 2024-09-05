8da2b855f2df2d243ad72d81c4eabb214527a4cf
pcbarretos <paullo.barreto@gmail.com>
2024-09-05 13:07:45 -0300

    Update

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,9 +2,4 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
+data "aws_availability_zones" "available" {}
